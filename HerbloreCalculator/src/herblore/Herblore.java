ba45050594bb405e10fc1e617238deeea5541e8c
James Voong <voongjame@ecs.vuw.ac.nz>
2016-11-07 01:54:00 +1300

    Deleted WebPageFetcher
    Restructured code for Herblore. Added more accurate calculation methods

--- HerbloreCalculator/src/herblore/Herblore.java
+++ HerbloreCalculator/src/herblore/Herblore.java
@@ -2,92 +2,131 @@ package herblore;
 
 public class Herblore {
 	private static int amount = 10000;
-	private static int grimy = 3760;
-	private static int clean = 3925;
-	private static int unf = 4487;
-	private static int vial = 58;
-	private static int secondary = 9;
-	private static int dose3 = 3875;
-	private static int dose4 = 5167;
+	private static int grimy = 2756;
+	private static int clean = 2995;
+	private static int unf = 3240;
+	private static int vial = 52;
+	private static int secondary = 10;
+	private static int dose3 = 2630;
+	private static int dose4 = 3699;
 
 	public static void fromGrimy() {
-		int profit = (clean - grimy) * amount;
+		/** Calculate how much seed money will be required */
 		int seed = amount * grimy;
+
+		/** Calculate the profit from cleaning the herb */
+		int profit = (clean - grimy) * amount;
+
 		System.out.println("From Grimy:");
 		System.out.println("Total seed money for " + amount + " herbs:" + seed);
-		System.out.println("Profit from cleaning:" + profit);
+		System.out.println("Profit from cleaning: " + profit);
 
-		int extras = (int) (amount * 1.11) - amount;
-		int extraVials = extras * vial;
-		int SoC = (int) (unf * extras) - extraVials;
-		// System.out.println(SoC);
-		int unfProf = ((unf - (grimy + vial)) * amount);
-		System.out.println("Profit from making unf:" + (unfProf + SoC));
-
-		int cost = (grimy + vial + secondary) * amount;
-		int savedSecondary = extras * secondary;
-		int extra3 = (int) (dose3 * amount * 0.05);
-		int outputOf3 = (int) (dose3 * amount * 0.9);
-		int outputOf4 = (int) (dose4 * amount * 0.1);
-		int totalPots = extra3 + outputOf3 + outputOf4;
-		int potProf = totalPots - cost + savedSecondary;
-		System.out.println("Total seed money for " + amount + " potions:" + cost);
-		System.out.println("Profit from making potion: " + potProf);
-		int converted4 = (int) (((amount * 0.1 * 4) / 3) * dose3);
-		int total3 = outputOf3 + converted4;
-		int potProf2 = total3 + extra3 + savedSecondary - cost;
-		System.out.println("Profit after converting 4dose: " + potProf2);
+		/** Call fromClean to do rest of the calculations */
+		fromClean(grimy + vial);
 	}
 
-	public static void fromClean() {
-		int seed = clean * amount;
-		System.out.println("\nFrom Clean:");
-		System.out.println("Total seed money for " + amount + " herbs:" + seed);
+	public static void fromClean(int rawCostOfPrimaryIngredient) {
+		if (rawCostOfPrimaryIngredient == (clean + vial)) {
+			int seed = rawCostOfPrimaryIngredient * amount;
+			System.out.println("\nFrom Clean:");
+			System.out.println("Total seed money for " + amount + " herbs:" + seed);
+		}
 
-		int extras = (int) (amount * 1.11) - amount;
-		int extraVials = extras * vial;
-		int SoC = (int) (unf * extras) - extraVials;
-		int unfProf = ((unf - (clean + vial)) * amount);
-		System.out.println("Profit from making unf:" + (unfProf + SoC));
+		/** Calculate net profit of making unf */
+		int extrasFromScroll = (int) (amount * 1.11) - amount;
+		int valueOfExtraVials = extrasFromScroll * vial;
+		int valueOfExtraUnfsFromScroll = (int) (unf * extrasFromScroll) - valueOfExtraVials;
+		int unfNetProfit = ((unf - rawCostOfPrimaryIngredient) * amount) + valueOfExtraUnfsFromScroll;
+		System.out.println("Profit from making unf:" + (unfNetProfit));
 
-		int cost = (clean + vial + secondary) * amount;
-		int savedSecondary = extras * secondary;
-		int extra3 = (int) (dose3 * amount * 0.05);
-		int outputOf3 = (int) (dose3 * amount * 0.9);
-		int outputOf4 = (int) (dose4 * amount * 0.1);
-		int totalPots = extra3 + outputOf3 + outputOf4;
-		int potProf = totalPots - cost + savedSecondary;
-		System.out.println("Total seed money for " + amount + " potions:" + cost);
-		System.out.println("Profit from making potion: " + potProf);
-		int converted4 = (int) (((amount * 0.1 * 4) / 3) * dose3);
-		int total3 = outputOf3 + converted4;
-		int potProf2 = total3 + extra3 + savedSecondary - cost;
-		System.out.println("Profit after converting 4dose: " + potProf2);
+		/** Call fromUnf to do the remaining calculations of profit */
+		int valueFromMakingPot = fromUnf(rawCostOfPrimaryIngredient + vial);
+		System.out.println("NB there will still be remaining unfs, value of which will be " + valueOfExtraUnfsFromScroll
+				+ ", bringing total net profit to " + (valueFromMakingPot + valueOfExtraUnfsFromScroll));
+
 	}
 
-	public static void fromUnf() {
-		System.out.println("\nFrom Unf:");
+	public static int fromUnf(int rawCostOfPrimaryIngredient) {
+		if (rawCostOfPrimaryIngredient == unf) {
+			System.out.println("\nFrom Unf:");
+		}
+		/** Extra secondary ingredients saved from the Scroll of Cleansing */
+		double extraSecondariesFromScroll = (amount * 1.11) - amount;
 
-		int extras = (int) (amount * 1.11) - amount;
-		int cost = (unf + secondary) * amount;
-		int savedSecondary = extras * secondary;
-		int extra3 = (int) (dose3 * amount * 0.05);
-		int outputOf3 = (int) (dose3 * amount * 0.9);
-		int outputOf4 = (int) (dose4 * amount * 0.1);
-		int totalPots = extra3 + outputOf3 + outputOf4;
-		int potProf = totalPots - cost + savedSecondary;
-		System.out.println("Total seed money for " + amount + " potions:" + cost);
-		System.out.println("Profit from making potion: " + potProf);
-		int converted4 = (int) (((amount * 0.1 * 4) / 3) * dose3);
-		int total3 = outputOf3 + converted4;
-		int potProf2 = total3 + extra3 + savedSecondary - cost;
-		System.out.println("Profit after converting 4dose: " + potProf2);
+		/**
+		 * Total seed money required to buy all the Unfs and secondary
+		 * ingredients
+		 */
+		int costToPurchaseRawMaterials = (rawCostOfPrimaryIngredient + secondary) * amount;
+		System.out.println("Total seed money for " + amount + " potions:" + costToPurchaseRawMaterials);
+
+		/** The amount from reselling the extra secondary ingredients */
+		int valueOfSavedSecondaryFromScroll = (int) extraSecondariesFromScroll * secondary;
+
+		/** The amount of extra 3 doses from using the well */
+		int valueOfExtra3DosePotsFromWell = (int) ((amount * 0.05) * dose3);
+
+		/**
+		 * The values of 3 and 4 dose from using the factory outfit. 10% will be
+		 * 4 converted to 4 dose pots
+		 */
+		int totalValueOf3DosePots = (int) ((amount * 0.9) * dose3);
+		int totalValueOf4DosePots = (int) ((amount * 0.1) * dose4);
+
+		/** The total value of selling the remaining potions */
+		int totalValuePots = valueOfExtra3DosePotsFromWell + totalValueOf3DosePots + totalValueOf4DosePots;
+
+		/** The net profit */
+		int potProfit = totalValuePots - costToPurchaseRawMaterials + valueOfSavedSecondaryFromScroll;
+
+		System.out.println("Profit from making potion without decanting: " + potProfit);
+
+		/**
+		 * Converting all potions into 3 dose pots and working out the gross
+		 * profit
+		 */
+		int valueGeneratedFromConvertingInto3Dose = (int) (((amount * 0.1 * 4) / 3) * dose3);
+		int totalValueAfterConvertingAllTo3Dose = totalValueOf3DosePots + valueGeneratedFromConvertingInto3Dose;
+
+		/** Work out the net profit from converting into 3 dose pots */
+		int potNetProfitFromConvertingTo3Dose = totalValueAfterConvertingAllTo3Dose + valueOfExtra3DosePotsFromWell
+				+ valueOfSavedSecondaryFromScroll - costToPurchaseRawMaterials;
+
+		/**
+		 * Converting all potions into 4 dose pots and working out the gross
+		 * profit
+		 */
+		int totalNumberOf3Dose = (int) ((amount * 0.05) + (amount * 0.9));
+		int valueGeneratedFromConvertingInto4Dose = (int) (((totalNumberOf3Dose * 3) / 4) * dose4);
+		int totalValueAfterConvertingAllTo4Dose = totalValueOf4DosePots + valueGeneratedFromConvertingInto4Dose;
+
+		/** Work out the net profit from converting into 4 dose pots */
+		int potNetProfitFromConvertingTo4Dose = totalValueAfterConvertingAllTo4Dose + valueOfSavedSecondaryFromScroll
+				- costToPurchaseRawMaterials;
+
+		/** Check to see which generates a greater profit and inform the user */
+		if (potNetProfitFromConvertingTo3Dose > potNetProfitFromConvertingTo4Dose) {
+			System.out.println("Profit is greater after converting to 3 dose by "
+					+ (potNetProfitFromConvertingTo3Dose - potNetProfitFromConvertingTo4Dose)
+					+ ", bringing the net profit to " + potNetProfitFromConvertingTo3Dose);
+
+			return (potNetProfitFromConvertingTo3Dose);
+		} else {
+			System.out.println("Profit is greater after converting to 4 dose by "
+					+ (potNetProfitFromConvertingTo4Dose - potNetProfitFromConvertingTo3Dose)
+					+ ", bringing the net profit to " + potNetProfitFromConvertingTo4Dose);
+			return (potNetProfitFromConvertingTo4Dose);
+		}
+	}
+
+	public static void doAllCalculations() {
+		fromGrimy();
+		fromClean(clean + vial);
+		fromUnf(unf);
 	}
 
 	public static void main(String[] args) {
-		fromGrimy();
-		fromClean();
-		fromUnf();
+		doAllCalculations();
 	}
 
 }
