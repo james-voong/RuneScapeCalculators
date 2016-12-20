a16117436730bffed8a459ec4ec1def346747043
James Voong <voongjame@ecs.vuw.ac.nz>
2016-12-20 20:19:55 +1300

    Updated with Scroll of Efficiency support

--- HerbloreCalculator/src/smithing/Metal.java
+++ HerbloreCalculator/src/smithing/Metal.java
@@ -4,17 +4,16 @@ import parser.Parser;
 
 public abstract class Metal {
 	private String barName, itemName;
-	private int orePrice, barPrice, oreNumber, barsPerItem, itemSellPrice;
+	private int orePrice, barPrice, coalNumber, barsPerItem, itemSellPrice;
 
 	private static int coalPrice = Parser
 			.getGrandExchangePrice("http://services.runescape.com/m=itemdb_rs/a=13/Coal/viewitem?obj=453");
 
-	public Metal(String barName, String ore, int oreNumber, String itemName, int barsPerItem) {
-
+	public Metal(String barName, String ore, int coalNumber, String itemName, int barsPerItem) {
 		this.barName = Parser.getNameOfItem(barName);
 		this.orePrice = Parser.getGrandExchangePrice(ore);
 		this.barPrice = Parser.getGrandExchangePrice(barName);
-		this.oreNumber = oreNumber;
+		this.coalNumber = coalNumber;
 		this.barsPerItem = barsPerItem;
 		this.itemName = Parser.getNameOfItem(itemName);
 		this.itemSellPrice = Parser.getGrandExchangePrice(itemName);
@@ -23,46 +22,104 @@ public abstract class Metal {
 
 	public void printNetProfitFromSmeltingBar(int amount) {
 		int netProfit = calculateNetProfitFromSmeltingBar(amount);
-		System.out.println("Net profit from smelting " + amount + " " + barName + "s: " + netProfit);
+		System.out.println("\nNet profit from smelting " + amount + " " + barName + "s: " + netProfit);
 	}
 
 	public void printNetProfitSmithingFromOre(int amount) {
 		int netProfit = calculateNetProfitSmithingFromOre(amount);
-		System.out.println("Net profit from smithing " + amount + " " + itemName + "s: " + netProfit);
+		System.out.println("\nNet profit from smithing " + amount + " " + itemName + "s: " + netProfit);
 	}
 
 	public void printNetProfitFromSmithingBars(int amount) {
-		int netProfit = calculateNetProfitFromSmithingBars(amount);
-		System.out.println("Net profit from smithing " + amount + " " + itemName + "s: " + netProfit);
+		int netProfit = calculateNetProfitFromSmithingBars(amount, 0, 0);
+		System.out.println("\nNet profit from smithing " + amount + " " + itemName + "s: " + netProfit);
 	}
 
+	/** Calculates net profit of buying ore then selling the bar */
 	private int calculateNetProfitFromSmeltingBar(int amount) {
-		int coalCost = oreNumber * coalPrice;
-		int barCost = coalCost + orePrice;
+		int barCost = coalNumber * coalPrice + orePrice;
 		int netProfit = barPrice - barCost;
 		int netProfitWithPortable = (int) ((netProfit + 0.05 * barPrice) * amount);
 		return netProfitWithPortable;
 	}
 
+	/** Calculates net profit of buying ore, making bars then smithing them */
 	private int calculateNetProfitSmithingFromOre(int amount) {
-		int barCost = oreNumber * coalPrice + orePrice;
+
+		int barCost = coalNumber * coalPrice + orePrice;
 		int totalBarCost = barCost * amount * barsPerItem;
+
 		double extraBarsFromSmelting = amount * 0.05 * barPrice;
-		double extraBarsFromSmithing = amount * 0.1 * barPrice;
-		int extraBarsTotal = (int) (extraBarsFromSmelting + extraBarsFromSmithing);
-		int grossProfit = itemSellPrice * amount;
-		int netProfit = grossProfit - totalBarCost + extraBarsTotal;
+		// double extraBarsFromSmithing = amount * 0.1 * barPrice;
+
+		// int savingsFromScroll = getSavingsFromScroll(amount);
+
+		// int extraBarsTotal = (int) (extraBarsFromSmelting +
+		// extraBarsFromSmithing);
+
+		// int grossProfit = itemSellPrice * amount;
+		int netProfit = calculateNetProfitFromSmithingBars(amount, totalBarCost, extraBarsFromSmelting);
 
 		return netProfit;
 	}
 
-	private int calculateNetProfitFromSmithingBars(int amount) {
-		int barsCost = barPrice * amount * barsPerItem;
-		int extraBars = (int) (amount * 0.1 * barPrice);
+	/** Calculates net profit of buying bars and smithing them */
+	private int calculateNetProfitFromSmithingBars(int amount, int totalBarCost, double extraBarsFromSmelting) {
+		if (totalBarCost == 0) {
+			totalBarCost = barPrice * amount * barsPerItem;
+		}
+
+		double extraBarsFromSmithing = amount * 0.1 * barPrice;
+
+		// This accounts for the scroll of efficiency
+		int savingsFromScroll = getSavingsFromScroll(amount);
+
+		int extraBarsTotal = (int) (extraBarsFromSmelting + extraBarsFromSmithing);
 		int grossProfit = itemSellPrice * amount;
-		int netProfit = grossProfit - barsCost + extraBars;
+		int netProfit = grossProfit - totalBarCost + extraBarsTotal + savingsFromScroll;
 
 		return netProfit;
+	}
+
+	private String getMetalName() {
+		String[] metalName = new String[1];
+		metalName = barName.split(" ");
+
+		return metalName[0];
+	}
+
+	/** Returns the amount saved from the scroll of efficiency */
+	private int getSavingsFromScroll(int amount) {
+		if (barsPerItem < 3) {
+			return 0;
+		} else {
+			int savingsFromScroll = 0;
+			double percentageOfSaving = 0;
+			String metalName = getMetalName();
+			switch (metalName) {
+
+			case "Rune":
+				percentageOfSaving = 0.05;
+				break;
+			case "Adamant":
+				percentageOfSaving = 0.08;
+				break;
+			case "Mithril":
+				percentageOfSaving = 0.1;
+				break;
+			case "Steel":
+				percentageOfSaving = 0.2;
+				break;
+			case "Iron":
+				percentageOfSaving = 0.25;
+				break;
+			case "Bronze":
+				percentageOfSaving = 0.5;
+				break;
+			}
+			savingsFromScroll = (int) (amount * percentageOfSaving * barPrice);
+			return savingsFromScroll;
+		}
 	}
 
 }
--- HerbloreCalculator/src/test/Test.java
+++ HerbloreCalculator/src/test/Test.java
@@ -9,19 +9,20 @@ import smithing.Runite;
 public class Test {
 	public static void main(String[] args) {
 
-		TradablePotion.superAttack(17963).printPotionNetProfitFromUnf();
-		TradablePotion.superDefence(11976).printPotionNetProfitFromUnf();
-		TradablePotion.superStrength(14371).printPotionNetProfitFromUnf();
-		TradablePotion.prayerRenewal(9000).printPotionNetProfitFromUnf();
-		TradablePotion.prayerRestore(22582).printPotionNetProfitFromUnf();
-		TradablePotion.saradominBrew(10978).printPotionNetProfitFromUnf();
-		TradablePotion.zamorakBrew(11291).printPotionNetProfitFromUnf();
+		// TradablePotion.superAttack(17963).printPotionNetProfitFromUnf();
+		// TradablePotion.superDefence(11976).printPotionNetProfitFromUnf();
+		// TradablePotion.superStrength(14371).printPotionNetProfitFromUnf();
+		// TradablePotion.prayerRenewal(9000).printPotionNetProfitFromUnf();
+		// TradablePotion.prayerRestore(22582).printPotionNetProfitFromUnf();
+		// TradablePotion.saradominBrew(10978).printPotionNetProfitFromUnf();
+		// TradablePotion.zamorakBrew(11291).printPotionNetProfitFromUnf();
 
 		Runite.MedHelm().printNetProfitFromSmithingBars(1);
 		Adamant.PlateBody().printNetProfitFromSmithingBars(1);
 		Adamant.Claw().printNetProfitFromSmithingBars(1);
 
 		// Bond.doCalculation();
+
 	}
 
 }
